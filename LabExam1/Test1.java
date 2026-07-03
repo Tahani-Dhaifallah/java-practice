ccb24ddba72a38edb3a3d0086e6cf37a306df790
Tahani Almutairi <tahani8897@gmail.com>
2026-07-03 10:27:21 +0300

    Simplify date initialization in Test1 class
    Just try *-*

--- LabExam1/Test1.java
+++ LabExam1/Test1.java
@@ -3,7 +3,8 @@ package labexams.LabExam1;
 // @author Tahani Dhaifallah
 public class Test1 {
     public static void main(String[] args) {
-      Food f1=new Food(2,false,"standard","Sarah",new Date(2,10,2025),new Date(15,12,2025));
+        Date d1=new Date(2,10,2025);
+      Food f1=new Food(2,false,"standard","Sarah",d1,new Date(15,12,2025));
         System.out.println(f1.displyInfo()+"\n");
         Food f2=new Food(3,true,"Standard","Ahmed",new Date(2,10,2020),new Date(1,6,2025));
         System.out.println(f2.displyInfo()+"\n");  
